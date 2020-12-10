904944d26104a16b77c8f352d0ec15f41eea0629
Daniela Luiza <danii.bnu@hotmail.com>
2020-12-10 18:02:17 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app> 
+    <div class="top">
+      <div class="avatar">
+        <v-avatar size="100">
+          <img
+            src="https://picsum.photos/200/300"
+            alt="John"
+          >
+        </v-avatar>
+      </div>
+      <div>
+        <H3>Daniela's PhotoGrid</H3>
+        <H5>I'm from Blumenau</H5>
+      </div>
+    </div>
+    <v-main>
+      <FotosIndex/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import FotosIndex from './components/FotosIndex';
+
+export default {
+  name: 'App',
+
+  components: {
+    FotosIndex,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style scoped>
+.top{
+  display: flex;
+  justify-content:space-evenly;
+  align-items: center;
+  height: 20vh;
+}
+</style>
---
+++ src/components/FotosIndex.vue
@@ -0,0 +1,69 @@
+<template>
+    <v-container class="container">
+        <ul>
+           <v-row>
+                <v-col
+                v-for="n in 12"
+                :key="n"
+                class="d-flex child-flex"
+                cols="4"
+                >
+                <v-card>
+                <v-img
+                    :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+                    :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                    aspect-ratio="1"
+                    class="grey lighten-2"
+                >
+                    <template v-slot:placeholder>
+                    <v-row
+                        class="fill-height ma-0"
+                        align="center"
+                        justify="center"
+                    >
+                        <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5"
+                        ></v-progress-circular>
+                    </v-row>
+                    </template>
+                </v-img>
+                 <v-card-subtitle size="1">
+                             <p class="text-caption"> Lorem ipsum </p>
+                    </v-card-subtitle>
+                </v-card>
+                </v-col>
+            </v-row>
+        </ul>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name:"FotosIndex",
+ /*   data(){
+        return{
+            imagens:[
+                {
+                id: 1,
+                foto: "https://picsum.photos/500/300", 
+                texto: "Lorem Ipsum",
+                }
+            ]
+        }
+    }*/
+}
+</script>
+
+<style scoped>
+.container{
+    display: flex;
+    justify-content: center;
+}
+/*.fotos{
+   width: 50vw;
+    display: flex;
+    justify-content: center;
+    background-color: aqua;
+}*/
+</style>
